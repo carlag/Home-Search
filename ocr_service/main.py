dbdbc61af8376b0f7c5428d98589102d53f0efe5
dgolding <dan@golding.za.net>
2020-12-20 00:25:51 +0000

    Added OCR support for PDFs

--- ocr_service/main.py
+++ ocr_service/main.py
@@ -1,10 +1,21 @@
+import logging
+
 from fastapi import FastAPI
 
-from ocr import get_area
+from ocr import get_area_jpg, get_area_pdf
 
+
+LOGGER = logging.getLogger()
 app = FastAPI()
 
 
-@app.get("/image/{image_file}")
+logging.basicConfig(level=logging.INFO)
+
+
+@app.get("/jpg/{image_file}")
 async def get_floorplan_area(image_file):
-    return {"area": get_area(f"https://lc.zoocdn.com/{image_file}")}
+    return {"area": get_area_jpg(f"https://lc.zoocdn.com/{image_file}.jpg")}
+
+@app.get("/pdf/{image_file}")
+async def get_floorplan_area(image_file):
+    return {"area": get_area_pdf(f"https://lc.zoocdn.com/{image_file}.pdf")}
--- ocr_service/ocr.py
+++ ocr_service/ocr.py
@@ -1,14 +1,30 @@
+import logging
 import re
 from io import BytesIO
 
 import pytesseract
 import requests
 from PIL import Image
+from pdf2image import convert_from_bytes
 
 
-def get_area(floorplan_url: str) -> float:
+LOGGER = logging.getLogger()
+PATTERN = re.compile(r"(\d*.?\d*) sq\s?m")
+
+def get_area_jpg(floorplan_url: str) -> float:
 
     image = requests.get(floorplan_url).content
     floorplan_text = pytesseract.image_to_string(Image.open(BytesIO(image)))
-    sqm = re.search(r"(\d*.?\d*) sq m", floorplan_text).groups()[0]
+    LOGGER.debug(f"OCR text:\n\n{floorplan_text}")
+    sqm = PATTERN.search(floorplan_text).groups()[0]
+    return float(sqm)
+
+
+def get_area_pdf(floorplan_url: str) -> float:
+
+    pdf = requests.get(floorplan_url).content
+    image = convert_from_bytes(pdf)[0]
+    floorplan_text = pytesseract.image_to_string(image)
+    LOGGER.debug(f"OCR text:\n\n{floorplan_text}")
+    sqm = PATTERN.search(floorplan_text).groups()[0]
     return float(sqm)
