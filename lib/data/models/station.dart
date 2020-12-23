f123d59ce89e42ebeca30bf2e294549149afc5ec
carla.goldstein@gmail.com <carlagoldstein@google.com>
2020-12-23 18:42:31 +0000

    Formatting

--- lib/data/models/station.dart
+++ lib/data/models/station.dart
@@ -1,10 +1,10 @@
 class Station {
-  double? distance;
-  double? time;
+  double? distance; // in metres
+  double? duration; //in seconds
   String? name;
 
   Station.fromJson(Map<String, dynamic> json)
       : name = json['address'] ?? 'No name',
         distance = json['distance'] ?? 'No distance',
-        time = json['duration'] ?? 'No time';
+        duration = json['duration'] ?? 'No time';
 }
--- lib/view/stations.dart
+++ lib/view/stations.dart
@@ -13,6 +13,11 @@ class Stations extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final titleStyle =
+        DefaultTextStyle.of(context).style.apply(fontSizeFactor: 0.8);
+    final subTitleStyle =
+        DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1.0);
+
     return FutureBuilder<List<Station>>(
       future: downloadData(),
       builder: (BuildContext context, AsyncSnapshot<List<Station>> snapshot) {
@@ -24,21 +29,46 @@ class Stations extends StatelessWidget {
             if (snapshot.hasError)
               return Text('Error: ${snapshot.error}', style: style);
             else
-              return Column(
+              return Table(
+                defaultColumnWidth: IntrinsicColumnWidth(),
                 children: List.generate(
                   snapshot.data?.length ?? 0,
                   (index) {
                     final station = snapshot.data![index];
-                    return ListTile(
-                      title: Text(station.name ?? 'Unknown station'),
-                      subtitle: Text('Distance: ${station.distance}, '
-                          'Walking: ${station.time} mins'),
-                    );
+                    return _station(station, titleStyle, subTitleStyle);
                   },
                 ),
               );
         }
       },
+    );
+  }
+
+  TableRow _station(Station station, TextStyle title, TextStyle subtitle) {
+    final accentedStyle =
+        TextStyle(color: Colors.blueAccent, fontStyle: subtitle.fontStyle);
+    var parts = station.name?.split(',') ?? ['Unknown name'];
+    var prefix = parts[0].trim();
+    return TableRow(
+      children: [
+        TableCell(
+          child: Text(prefix, style: subtitle),
+        ),
+        TableCell(
+          child: Text(' Distance: ', style: subtitle),
+        ),
+        TableCell(
+          child: Text('${(station.distance! / 1000.0).toStringAsFixed(2)} km, ',
+              style: accentedStyle),
+        ),
+        TableCell(
+          child: Text(' Walking: ', style: subtitle),
+        ),
+        TableCell(
+          child: Text('${(station.duration! / 60.0).toStringAsFixed(2)} mins',
+              style: accentedStyle),
+        ),
+      ],
     );
   }
 
