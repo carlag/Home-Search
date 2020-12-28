b0752f6e3e5cd1e4746c6fef844aab1ede0925a6
carla.goldstein@gmail.com <carlagoldstein@google.com>
2020-12-28 15:48:50 +0000

    updated .gitignore and added rules to replace google api key

--- flutter_server.sh
+++ flutter_server.sh
@@ -13,9 +13,14 @@ fuser -k 5001/tcp
 # Change directories to the release folder
 cd build/web/
 
+sed -i '.bkp' "s/<GOOGLEMAPSAPIKEY>/$GOOGLEMAPSAPIKEY/" index.html
+
 # Start the server
 echo 'Starting server on port' $PORT '...'
 python3 -m http.server $PORT
 
 # Exit
 echo 'Server exited...'
+
+mv index.html.bkp index.html
+cd ../..
