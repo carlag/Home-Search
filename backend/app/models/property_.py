d8dd9cc6a5af05c2fa342931ce67476284d87580
dgolding <dan@golding.za.net>
2021-01-24 19:00:40 +0000

    Removed mark from PropertyModel repr

--- backend/app/models/property_.py
+++ backend/app/models/property_.py
@@ -27,8 +27,7 @@ class PropertyModel(Base):
                 f"listing_id={self.listing_id}"
                 f", listing_url={self.listing_url}"
                 f", floorplan_url={self.floorplan_url}"
-                f", ocr_size={self.ocr_size}"
-                f", mark={self.mark})")
+                f", ocr_size={self.ocr_size}")
 
 
 class SavedModel(Base):
