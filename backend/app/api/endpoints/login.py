d542620fd7bc1c8f6a6558a7b06d6e8e0f6f93b7
dgolding <dan@golding.za.net>
2021-01-24 14:21:58 +0000

    Trying to make swap tokens endpoint accept whatever a Request is

--- backend/app/api/endpoints/login.py
+++ backend/app/api/endpoints/login.py
@@ -1,7 +1,8 @@
 import logging
 from typing import Any, Dict
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
+from fastapi.encoders import jsonable_encoder
 from requests import Session
 
 from app.api.deps import get_db
@@ -13,12 +14,16 @@ router = APIRouter()
 
 
 @router.post("/login/swap-tokens", response_model=Token)
-def login_access_token(*, db: Session = Depends(get_db), google_token) -> Dict[str, Any]:
+def login_access_token(*, db: Session = Depends(get_db), request: Request = None) -> Dict[str, Any]:
     """
     Authenticate a Google token, and return an access token for future requests
     """
     LOGGER.info("Swapping tokens")
-    user_email = get_email_from_google_token(google_token)
+    body_bytes = await request.body()
+    LOGGER.info(f"Reuest body bytes: {body_bytes}")
+    auth_code = jsonable_encoder(body_bytes)
+    LOGGER.info(f"Auth code: {auth_code}")
+    user_email = get_email_from_google_token(auth_code)
     LOGGER.info(f"(swap-tokens) user_email: {user_email}")
     # TODO: Add user to DB
 
