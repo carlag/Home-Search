da526f636702c8ccc24bc62fc29be17de9b67661
dgolding <dan@golding.za.net>
2021-01-31 22:06:01 +0000

    Removed db dep on floorplan api endoints. These will no longer cache

--- backend/app/api/endpoints/floorplan.py
+++ backend/app/api/endpoints/floorplan.py
@@ -1,10 +1,9 @@
 import logging
 from typing import Dict, Any, Callable
 
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException
 from sqlalchemy.orm import Session
 
-from app.api.deps import get_db
 from app.property_server import PropertyServer
 
 LOGGER = logging.getLogger()
@@ -13,21 +12,20 @@ property_server = PropertyServer(page_size=10)
 
 
 @router.get("/image/{image_file}")
-async def get_floorplan_area_image(*, db: Session = Depends(get_db), image_file: str) -> Dict[str, Any]:
-    return _get_area(db, image_file, property_server.floorplan_reader.get_area_image)
+async def get_floorplan_area_image(*, image_file: str) -> Dict[str, Any]:
+    return _get_area(image_file, property_server.floorplan_reader.get_area_image)
 
 
 @router.get("/pdf/{pdf_file}")
-async def get_floorplan_area_pdf(*, db: Session = Depends(get_db), pdf_file: str) -> Dict[str, Any]:
-    return _get_area(db, pdf_file, property_server.floorplan_reader.get_area_pdf)
+async def get_floorplan_area_pdf(*, pdf_file: str) -> Dict[str, Any]:
+    return _get_area(pdf_file, property_server.floorplan_reader.get_area_pdf)
 
 
-def _get_area(db: Session,
-              image_file: str,
+def _get_area(image_file: str,
               area_function: Callable[[Session, str], float]) -> Dict[str, Any]:
     try:
         url = f"https://lc.zoocdn.com/{image_file}"
-        area = area_function(db, url)
+        area = area_function(url)
         LOGGER.info(f"Area for request '{url}': '{area}'")
         return {"area": area}
     except ValueError as err:
