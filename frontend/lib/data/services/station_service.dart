bea506f18b42b9054b5ea97d4639f147b8a30963
carla.goldstein@gmail.com <carlagoldstein@google.com>
2021-01-31 22:40:46 +0000

    Removed / and added CORS

--- frontend/lib/data/services/station_service.dart
+++ frontend/lib/data/services/station_service.dart
@@ -16,7 +16,10 @@ class StationService {
       print('Request: $request');
       final response = await client.get(
         request,
-        headers: {"Access-Control-Allow-Origin": "*"},
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'POST,GET,DELETE,PUT,OPTIONS',
+        },
       );
       if (response.statusCode == 200) {
         final json = jsonDecode(response.body)['stations'] as List<dynamic>;
@@ -47,6 +50,8 @@ class StationService {
   Map<String, String> _headers() {
     final headers = <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'POST,GET,DELETE,PUT,OPTIONS',
     };
     return headers;
   }
